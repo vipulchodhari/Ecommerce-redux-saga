af7b8514f8d9b7f40d71ebdd05860c7043fb7a6e
vipulchodhari <110665652+vipulchodhari@users.noreply.github.com>
2022-08-16 10:36:31 +0530

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -19,10 +19,6 @@ export const Cart = () => {
     const RemoveCart = (el) => {
         dispatch(Remove_From_Cart(el))
     }
-    
-    const increaseQuantity = (id) => {
-
-    }
 
     let totalAmount = 0;
     let totalPrice = 0;
@@ -52,12 +48,7 @@ export const Cart = () => {
                             </div>
                             <hr />
                             <div className='cart-move'>
-                                <p onClick={() => RemoveCart(item)}><span><RiDeleteBin6Line /></span>&nbsp;REMOVE</p>
-                                <div className='cart-item-quantity'>
-                                    <button>-</button>
-                                    <input onChange={() => increaseQuantity(item.id)} type='number' placeholder='1'/>
-                                    <button>+</button>
-                                </div>    
+                                <p onClick={() => RemoveCart(item)}><span><RiDeleteBin6Line /></span>&nbsp;REMOVE</p> 
                             </div>
                         </div>
                         <div className='cart-vertical-line'></div>
